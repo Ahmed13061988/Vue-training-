c5665c55829ebfdab1833f8c5ed3a7dfea885d60
Ahmed Hussein <ahmedhussein@Ahmeds-MacBook-Pro.local>
2021-08-19 12:42:39 -0600

    addind async function to fetch

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 new Vue({
     el: '#app', 
     data: {
-        name : "ahmed", 
+        name : "Alex", 
         age: 31, 
+        email: "alex@gmail.com", 
+        gender: "male", 
+        picture: "",
         salary: 57000, 
         classes: ['blue', 'underline']
     }, 
     methods: {
-      sayMyName(){
-          return this.name
-      }, 
-
-      sayMySalary(){
-        return this.salary
-      }, 
-
-      putYourName(name){
-          return name + " "+this.age
-      },
-
-      sayHi(message){
-        return alert(message)
-      }
+     async changeUser(){
+       const res = await fetch('https://randomuser.me/api')
+       const { results } = await res.json()
+        
+       this.name = results[0].name.first
+       this.email = results[0].email
+       this.gender = results[0].gender
+       this.age = results[0].age  
+       this.salary = 55000; 
+       this.picture = results[0].picture.large
+     }
     }
 })
